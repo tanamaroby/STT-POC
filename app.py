c09b24693ee57ff3a0959d4d9423e8a55f299aea
Roby Tanama <54106843+tanamaroby@users.noreply.github.com>
2025-06-28 08:24:59 +0800

    Implements audio transcription with Whisper
    Sets up a Gradio interface for audio transcription
    using the OpenAI Whisper model.
    
    It loads the Whisper model and processor, creates a
    pipeline for automatic speech recognition, and defines
    a `transcribe` function that uses the pipeline to
    convert audio to text.

--- app.py
+++ app.py
@@ -1,7 +1,37 @@
 import gradio as gr
+import torch
+from transformers import AutoModelForSpeechSeq2Seq, AutoProcessor, pipeline
+from datasets import load_dataset
 
-def greet(name):
-    return "Hello " + name + "!!"
+device = "cuda:0" if torch.cuda.is_available() else "cpu"
+torch_dtype = torch.float16 if torch.cuda.is_available() else torch.float32
 
-demo = gr.Interface(fn=greet, inputs="text", outputs="text")
+model_id = "openai/whisper-large-v3"
+
+model = AutoModelForSpeechSeq2Seq.from_pretrained(
+    model_id, torch_dtype=torch_dtype, low_cpu_mem_usage=True, use_safetensors=True
+)
+
+model.to(device)
+
+processor = AutoProcessor.from_pretrained(model_id)
+
+pipe = pipeline(
+    "automatic_speech_recognition",
+    model=model,
+    tokenizer=processor.tokenizer,
+    feature_extractor=processor.feature_extractor,
+    torch_dtype=torch_dtype,
+    device=device,
+)
+
+dataset = load_dataset("distil-whisper/librispeech_long", "clean", split="validation")
+sample = dataset[0]["audio"]
+
+def transcribe(audio):
+    result = pipe(audio)
+    return result["text"]
+
+
+demo = gr.Interface(fn=transcribe, inputs=gr.Audio(), outputs="text")
 demo.launch()
